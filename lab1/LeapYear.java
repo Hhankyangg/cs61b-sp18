862fed38bbef945e4cea9128c0861324d7d41832
Hank <hankyang428@gmail.com>
2023-09-04 11:22:17 +0800

    Complete lab01

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -7,6 +7,7 @@ public class LeapYear {
      *  @param  year to be analyzed
      */
     public static boolean isLeapYear(int year) {
+        /** Return whether year is leap year */
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             return true;
         }
