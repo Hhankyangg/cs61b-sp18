03897a406027b394bf80a25317d92baea11d278e
Hank <hankyang428@gmail.com>
2023-11-30 16:20:52 +0800

    complete hw3

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,10 +1,12 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TestComplexOomage {
@@ -33,21 +35,45 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
+    @Test
+    public void testHashCode() {
+        List<Integer> param1 = new LinkedList<>();
+        param1.add(1);
+        param1.add(7);
+        param1.add(2);
+        param1.add(3);
+        param1.add(4);
+        param1.add(6);
+        ComplexOomage so1 = new ComplexOomage(param1);
+        List<Integer> param2 = new LinkedList<>();
+        param2.add(7);
+        param2.add(8);
+        param2.add(2);
+        param2.add(3);
+        param2.add(4);
+        param2.add(6);
+        ComplexOomage so2 = new ComplexOomage(param2);
+        assertEquals(so1.hashCode(), so2.hashCode());
+    }
+
+    /*
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
-        // Your code here.
-
+        for (int i = 0; i < 1000; i += 1) {
+            List<Integer> params = new ArrayList<>(8);
+            for (int j = 0; j < params.size(); j += 1) {
+                if (j < 4) {
+                    params.add(StdRandom.uniform(0, 256));
+                } else {
+                    params.add(j);
+                }
+            }
+            deadlyList.add(new ComplexOomage(params));
+        }
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
-
-    /** Calls tests for SimpleOomage. */
-    public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestComplexOomage.class);
     }
 }
--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -25,10 +25,11 @@ public class TestSimpleOomage {
 
     @Test
     public void testHashCodePerfect() {
-        /* TODO: Write a test that ensures the hashCode is perfect,
-          meaning no two SimpleOomages should EVER have the same
-          hashCode UNLESS they have the same red, blue, and green values!
-         */
+        SimpleOomage test1 = new SimpleOomage(0, 0, 200);
+        SimpleOomage test2 = new SimpleOomage(200, 0, 0);
+        SimpleOomage test3 = new SimpleOomage(0, 0, 200);
+        assertEquals(test1.hashCode(), test3.hashCode());
+        assertNotEquals(test1.hashCode(), test2.hashCode());
     }
 
     @Test
@@ -42,7 +43,7 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -50,10 +51,9 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
-    /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -63,10 +63,5 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
-
-    /** Calls tests for SimpleOomage. */
-    public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestSimpleOomage.class);
     }
 }
